7c0e5421ca2ed4eddc01cd8bbdcaaac334b549d0
Flanemir2020 <67554500+Flanemir2020@users.noreply.github.com>
2020-10-28 21:47:40 -0400

    pasta2

---
+++ calculadora_pronta/logica.js
@@ -0,0 +1,26 @@
+function clicar() {
+    var inp1 = document.querySelector(".inp1").value;
+    var inp2 = document.querySelector(".inp2").value;
+    var resultado = parseInt(inp1) + parseInt(inp2);
+    alert("Resultado da soma é: " + resultado);
+    }
+    function clicar1() {
+        var inp1 = document.querySelector(".inp1").value;
+        var inp2 = document.querySelector(".inp2").value;
+        var resultado = parseInt(inp1) - parseInt(inp2);
+        alert("Resultado da subtração é: " + resultado);
+        }
+        function clicar2() {
+            var inp1 = document.querySelector(".inp1").value;
+            var inp2 = document.querySelector(".inp2").value;
+            var resultado = parseInt(inp1) / parseInt(inp2);
+            alert("Resultado da divisão é: " + resultado);
+            }
+            function clicar3() {
+                var inp1 = document.querySelector(".inp1").value;
+                var inp2 = document.querySelector(".inp2").value;
+                var resultado = parseInt(inp1) * parseInt(inp2);
+                alert("Resultado da multiplicação é: " + resultado);
+                }
+
+    
